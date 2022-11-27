0679b77198a54dca264b66ddf9e95c18c4fbf3ac
lluis <lluis.cerda@estudiantat.upc.edu>
2022-11-27 17:46:44 +0100

    v4

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const scrollContainer = document.querySelector("main");
+var windowWidth = window.innerWidth;
+var horizontalLength = document.querySelector(".element-wrapper").scrollWidth;
 
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
+var distFromTop = document.querySelector(".horizontal-section").offsetTop;
+
+var scrollDistance = distFromTop + horizontalLength - windowWidth;
+
+document.querySelector(".horizontal-section").style.height = horizontalLength + "px";
+
+window.onscroll = function() {
+    var scrollTop = window.pageYOffset;
+
+    if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
+        document.querySelector(".element-wrapper").style.transform = "translateX(-"+(scrollTop - distFromTop)+"px";
+    }
+}
