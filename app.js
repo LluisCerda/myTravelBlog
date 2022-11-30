93465205b22ed0912065fcebd3244ddfdf874971
lluis <lluis.cerda@estudiantat.upc.edu>
2022-11-30 15:45:15 +0100

    Cool animated photo effect

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-var windowWidth = window.innerWidth;
-var horizontalLength = document.querySelector(".element-wrapper").scrollWidth;
+let images = [...document.querySelectorAll('.img')];
+let slider = document.querySelector('.slider');
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = .05;
 
-var distFromTop = document.querySelector(".horizontal-section").offsetTop;
+window. addEventListener('resize', init);
 
-var scrollDistance = distFromTop + horizontalLength - windowWidth;
+images.forEach((img, idx) => {
+    img.style.backgroundImage = `url(./images/${idx+1}.jpeg)`
+})
 
-document.querySelector(".horizontal-section").style.height = horizontalLength + "px";
-
-window.onscroll = function() {
-    var scrollTop = window.pageYOffset;
-
-    if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
-        document.querySelector(".element-wrapper").style.transform = "translateX(-"+(scrollTop - distFromTop)+"px";
-    }
+function lerp(start, end, t) {
+    return start * (1-t) + end * t;
 }
+
+function setTransform(el, transform) {
+    el.style.transform = transform;
+}
+
+function init() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth/ images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+}
+
+function animate() {
+    current = parseFloat(lerp(current, target, ease).toFixed(2));
+    target = window.scrollY;
+    setTransform(slider, `translateX(-${current}px)`);
+    animateImages();
+    requestAnimationFrame(animate);
+}
+
+function animateImages() {
+    let ratio = current / imageWidth;
+    let intersectionRatio;
+
+    images.forEach((image, idx) => {
+        intersectionRatio = ratio - (idx * 0.7);
+        setTransform(image, `translateX(${intersectionRatio * 70}px)`)
+    })
+}
+
+init();
+animate();
